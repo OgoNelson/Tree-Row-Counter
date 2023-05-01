fd57415d90c3aa93effcfbd9b1f5ab964c136fba
OgoNelson <116231998+OgoNelson@users.noreply.github.com>
2023-05-01 23:43:46 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+let count = 0
+
+function increment() {
+    //count = count + 1
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    enteries = count + " - "
+    saveEl.textContent += enteries
+    countEl.textContent = 0
+    count = 0
+}
